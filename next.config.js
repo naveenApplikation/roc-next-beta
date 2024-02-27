33b5c8b274486c3c5188786e56a470da3c438fd3
naveennkViwell <136687846+naveennkViwell@users.noreply.github.com>
2024-02-27 12:55:22 +0530

    feat: next config correction

--- next.config.js
+++ next.config.js
@@ -1,14 +1,20 @@
 module.exports = {
-    async rewrites() {
-      return [
-        {
-          source: '/community/:slug*',
-          destination: '/screens/community/:slug*', // Redirect to the original URL
-        },
-        {
-          source: '/categories/:slug*',
-          destination: '/screens/categories/:slug*', // Redirect to the original URL
-        },
-      ];
-    },
-  };
+  async rewrites() {
+    return [
+      {
+        source: "/community/:slug*",
+        destination: "/screens/community/:slug*", // Redirect to the original URL
+      },
+      {
+        source: "/categories/:slug*",
+        destination: "/screens/categories/:slug*", // Redirect to the original URL
+      },
+    ];
+  },
+};
+
+const nextConfig = {
+  output: "export",
+};
+
+module.exports = nextConfig;
