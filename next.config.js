e42ea77afc61ed39166dd5f44075b5fd3cfbea60
naveennkViwell <136687846+naveennkViwell@users.noreply.github.com>
2024-02-27 14:26:11 +0530

    feat: next config correction

--- next.config.js
+++ next.config.js
@@ -1,20 +1,28 @@
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: "/community/:slug*",
-        destination: "/screens/community/:slug*", // Redirect to the original URL
-      },
-      {
-        source: "/categories/:slug*",
-        destination: "/screens/categories/:slug*", // Redirect to the original URL
-      },
-    ];
-  },
+/** @type {import('next').NextConfig} */
+
+const rewrites = () => {
+  return [
+    {
+      source: "/community/:slug*",
+      destination: "/screens/community/:slug*", // Redirect to the original URL
+    },
+    {
+      source: "/categories/:slug*",
+      destination: "/screens/categories/:slug*", // Redirect to the original URL
+    },
+  ];
 };
 
 const nextConfig = {
   output: "export",
+  reactStrictMode: false,
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  rewrites: rewrites,
 };
 
-module.exports = nextConfig;
+export default nextConfig;
