0b38ec6d98e7802c08bb4557968274e95d429c38
Bharat Patel <patelb598@gmail.com>
2024-05-16 18:31:12 +0530

    icon name list

--- src/app/pageLayout.tsx
+++ src/app/pageLayout.tsx
@@ -42,7 +42,7 @@ const PageLayout: React.FC<PageLayoutProps> = ({ children }) => {
             <MainContainer>
                 {children}
             </MainContainer>
-            {showMap && <MapWithMenu />}
+            {/* {showMap && <MapWithMenu />} */}
         </Container>
     )
 }
