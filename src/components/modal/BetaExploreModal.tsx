b00e6b8c23065e8ab4abb24c240f7124a1b12774
work.technotoil <work.technotoil@gmail.com>
2024-05-16 16:18:22 +0530

    explore ROC

--- src/components/modal/BetaExploreModal.tsx
+++ src/components/modal/BetaExploreModal.tsx
@@ -2,6 +2,7 @@ import { useMyContext } from '@/app/Context/MyContext';
 // import MyListModalLayout from "@/components//modal/Modal";
 import WalkListModal from "@/components/modal/Modal";
 import React from 'react';
+import CommonButton from '../button/CommonButton';
 
 interface WalksModalProps {
     // Define your props here
@@ -21,6 +22,12 @@ const BetaExploreModal: React.FC<WalksModalProps> = (props) => {
             name="walksModal">
             <div style={{ height: "84vh", fontSize: "16px", width: "100%", overflow: 'hidden', padding: "20px" }}>
                 <p>As we fine-tune our features, your feedback is invaluable. While not everything may be fully functional yet, your participation is key in shaping the future of our platform. Thank you for being part of this journey with us.</p>
+                <div
+                    style={{ padding: "20px 0px" }}
+                    onClick={() => closeModal("")}
+                >
+                    <CommonButton text="Explore ROC" />
+                </div>
             </div>
         </WalkListModal>
     );
