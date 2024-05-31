7d4f56b758f65ac52aa243d34989bf194d3d949a
naveennkViwell <136687846+naveennkViwell@users.noreply.github.com>
2024-05-31 13:09:26 +0530

    feat: color correction and seach filter merge

--- src/components/googleMap/page.tsx
+++ src/components/googleMap/page.tsx
@@ -1,253 +1,255 @@
 import { useMyContext } from "@/app/Context/MyContext";
-import fallback from '../../../assets/images/fallbackimage.png';
+import fallback from "../../../assets/images/fallbackimage.png";
 import { TiInfo } from "react-icons/ti";
 import {
-    GoogleMap,
-    InfoWindow,
-    Marker,
-    useJsApiLoader,
+  GoogleMap,
+  InfoWindow,
+  Marker,
+  useJsApiLoader,
 } from "@react-google-maps/api";
 
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import toast from "react-hot-toast";
 
 interface GoogleMapCompProps {
-    // Define your props here
+  // Define your props here
 }
 const containerStyle = {
-    width: "400px",
-    height: "400px",
+  width: "400px",
+  height: "400px",
 };
 
 const center = {
-    lat: 49.1811261,
-    lng: -2.1051429,
+  lat: 49.1811261,
+  lng: -2.1051429,
 };
 
 const GoogleMapComp: React.FC<GoogleMapCompProps> = (props) => {
-    const { dataDetails, modalName, modalClick, location } = useMyContext();
-    const { isLoaded } = useJsApiLoader({
-        id: "google-map-script",
-        googleMapsApiKey: "AIzaSyAqHi-MH3gDZ0uCWYJL9w6Bi0iHtO_Kzx0",
+  const { dataDetails, modalName, modalClick, location } = useMyContext();
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyAqHi-MH3gDZ0uCWYJL9w6Bi0iHtO_Kzx0",
+  });
+
+  const [selectedLat, setSelectedLat] = useState<number>(49.1811261);
+  const [selectedLong, setSelectedLong] = useState<number>(2.1051429);
+  const [zoom, setZoom] = useState(15);
+  const mapRef = useRef<any>(null);
+
+  const [map, setMap] = React.useState(null);
+  const [markerLocation, setMarkerLocation] = useState(center);
+  const [selectedPlace, setSelectedPlace] = useState<any>({});
+  const [userLocation, setUserLocation] = useState(null);
+
+  // const jerseyBounds = new window.google.maps.LatLngBounds(
+  //     new window.google.maps.LatLng(49.1692, -2.2666),
+  //     new window.google.maps.LatLng(49.2668, -2.0116)
+  // );
+
+  // const checkIfInsideJersey = (location: any) => {
+  //     return jerseyBounds.contains(new window.google.maps.LatLng(location.lat, location.lng));
+  // };
+
+  // useEffect(() => {
+  //     if (navigator.geolocation) {
+  //         navigator.geolocation.getCurrentPosition(
+  //             (position) => {
+  //                 const { latitude, longitude } = position.coords;
+  //                 const userLoc: any = { lat: latitude, lng: longitude };
+  //                 const insideJersey = checkIfInsideJersey(userLoc);
+  //                 if(insideJersey){
+  //                     setUserLocation(userLoc);
+  //                 } else {
+  //                     toast.error('User is outside Jersey')
+  //                 }
+  //             },
+  //             (error) => {
+  //                 console.error('Error getting user location:', error);
+  //             }
+  //         );
+  //     }
+  // }, []);
+  const handleMapLoad = useCallback((map: any) => {
+    mapRef.current = map;
+
+    // Define Jersey Island boundaries
+    const jerseyBounds = new window.google.maps.LatLngBounds(
+      new window.google.maps.LatLng(49.1692, -2.2666),
+      new window.google.maps.LatLng(49.2668, -2.0116)
+    );
+
+    const checkIfInsideJersey = (location: any) => {
+      return jerseyBounds.contains(
+        new window.google.maps.LatLng(location.lat, location.lng)
+      );
+    };
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          const userLoc: any = { lat: latitude, lng: longitude };
+          // setUserLocation(userLoc);
+          const insideJersey = checkIfInsideJersey(userLoc);
+          console.log("User is inside Jersey:", insideJersey);
+          if (insideJersey) {
+            setUserLocation(userLoc);
+          } else {
+            toast.custom(
+              <div
+                style={{
+                  width: "150px",
+                  background: "white",
+                  display: "flex",
+                  gap: "10px",
+                  borderRadius: "10px",
+                  padding: "10px",
+                  boxSizing: "border-box",
+                  color: "#000000",
+                }}>
+                <TiInfo style={{ fontSize: "14px" }} />
+                <div style={{ color: "#000000" }}>User is outside Jersey</div>
+              </div>
+            );
+          }
+        },
+        (error) => {
+          console.error("Error getting user location:", error);
+        }
+      );
+    }
+  }, []);
+
+  useEffect(() => {
+    // if (window.location.pathname.includes("categories")) {
+    console.log(
+      "googlegooglegooglegooglegoogle",
+      dataDetails?.geometry,
+      location
+    );
+    setSelectedLat(
+      dataDetails?.data_type === "google"
+        ? dataDetails?.geometry?.location?.lat
+        : dataDetails?.acf?.map_location_lat
+        ? +dataDetails?.acf?.map_location_lat
+        : 49.1811261
+    );
+    setSelectedLong(
+      dataDetails?.data_type === "google"
+        ? dataDetails?.geometry?.location?.lng
+        : dataDetails?.acf?.map_location_lng
+        ? +dataDetails?.acf?.map_location_lng
+        : -2.1051429
+    );
+    // }
+  }, [
+    dataDetails?.acf?.map_location_lat,
+    dataDetails?.acf?.map_location_lng,
+    dataDetails?.geometry?.location?.lat,
+    dataDetails?.geometry?.location?.lng,
+  ]);
+
+  useEffect(() => {
+    setSelectedLat(49.1811261);
+    setSelectedLong(-2.1051429);
+  }, [window.location.pathname]);
+
+  const onUnmount = React.useCallback(function callback(map: any) {
+    setMap(null);
+  }, []);
+
+  const onMapClick = (e: any) => {
+    setMarkerLocation({
+      lat: e.latLng.lat(),
+      lng: e.latLng.lng(),
     });
+  };
 
-    const [selectedLat, setSelectedLat] = useState<number>(49.1811261);
-    const [selectedLong, setSelectedLong] = useState<number>(2.1051429);
-    const [zoom, setZoom] = useState(15);
-    const mapRef = useRef<any>(null);
+  const handleMarkerClick = (place: any) => {
+    setSelectedPlace(place);
+  };
 
-    const [map, setMap] = React.useState(null);
-    const [markerLocation, setMarkerLocation] = useState(center);
-    const [selectedPlace, setSelectedPlace] = useState<any>({});
-    const [userLocation, setUserLocation] = useState(null);
+  const handleInfoWindowClose = () => {
+    setSelectedPlace(null);
+  };
 
-    // const jerseyBounds = new window.google.maps.LatLngBounds(
-    //     new window.google.maps.LatLng(49.1692, -2.2666),
-    //     new window.google.maps.LatLng(49.2668, -2.0116)
-    // );
+  const handleZoomChanged = () => {
+    if (mapRef.current) {
+      setZoom(mapRef.current.getZoom());
+    }
+  };
 
-    // const checkIfInsideJersey = (location: any) => {
-    //     return jerseyBounds.contains(new window.google.maps.LatLng(location.lat, location.lng));
-    // };
-
-    // useEffect(() => {
-    //     if (navigator.geolocation) {
-    //         navigator.geolocation.getCurrentPosition(
-    //             (position) => {
-    //                 const { latitude, longitude } = position.coords;
-    //                 const userLoc: any = { lat: latitude, lng: longitude };
-    //                 const insideJersey = checkIfInsideJersey(userLoc);
-    //                 if(insideJersey){
-    //                     setUserLocation(userLoc);
-    //                 } else {
-    //                     toast.error('User is outside Jersey')
-    //                 }
-    //             },
-    //             (error) => {
-    //                 console.error('Error getting user location:', error);
-    //             }
-    //         );
-    //     }
-    // }, []);
-    const handleMapLoad = useCallback((map: any) => {
-        mapRef.current = map;
-
-        // Define Jersey Island boundaries
-        const jerseyBounds = new window.google.maps.LatLngBounds(
-            new window.google.maps.LatLng(49.1692, -2.2666),
-            new window.google.maps.LatLng(49.2668, -2.0116)
-        );
-
-        const checkIfInsideJersey = (location: any) => {
-            return jerseyBounds.contains(new window.google.maps.LatLng(location.lat, location.lng));
-        };
-
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-                (position) => {
-                    const { latitude, longitude } = position.coords;
-                    const userLoc: any = { lat: latitude, lng: longitude };
-                    // setUserLocation(userLoc);
-                    const insideJersey = checkIfInsideJersey(userLoc);
-                    console.log('User is inside Jersey:', insideJersey);
-                    if (insideJersey) {
-                        setUserLocation(userLoc);
-                    } else {
-                        toast.custom(
-                            <div style={{ width: '150px', background: 'white', display: 'flex', gap: '10px', borderRadius: '10px', padding: '10px', boxSizing: 'border-box' }}>
-                                <TiInfo style={{ fontSize: '14px', color: '#FF5733' }} />
-                                <div>User is outside Jersey</div>
-                            </div>)
-                    }
-                },
-                (error) => {
-                    console.error('Error getting user location:', error);
-                }
-            );
-        }
-    }, []);
-
-
-
-
-
-
-
-    useEffect(() => {
-        // if (window.location.pathname.includes("categories")) {
-        console.log("googlegooglegooglegooglegoogle", dataDetails?.geometry, location)
-        setSelectedLat(
-            dataDetails?.data_type === "google"
-                ?
-                (
-                    dataDetails?.geometry?.location?.lat
-                )
-                :
-                (dataDetails?.acf?.map_location_lat
-                    ? +dataDetails?.acf?.map_location_lat
-                    : 49.1811261)
-        );
-        setSelectedLong(
-            dataDetails?.data_type === "google" ?
-                (
-                    dataDetails?.geometry?.location?.lng
-                )
-                :
-                (dataDetails?.acf?.map_location_lng
-                    ? +dataDetails?.acf?.map_location_lng
-                    : -2.1051429)
-        );
-        // }
-    }, [(dataDetails?.acf?.map_location_lat), (dataDetails?.acf?.map_location_lng), (dataDetails?.geometry?.location?.lat), (dataDetails?.geometry?.location?.lng)]);
-
-    useEffect(() => {
-        setSelectedLat(49.1811261);
-        setSelectedLong(-2.1051429);
-    }, [window.location.pathname]);
-
-    const onUnmount = React.useCallback(function callback(map: any) {
-        setMap(null);
-    }, []);
-
-    const onMapClick = (e: any) => {
-        setMarkerLocation({
-            lat: e.latLng.lat(),
-            lng: e.latLng.lng(),
-        });
+  const markerClick = (e: any) => {
+    // console.log("data of map", e)
+  };
+  const handleClick = async (e: any) => {
+    const data = {
+      data_type: "google",
+      place_id: e.placeId,
+      lat: e.latLng.lat(),
+      lng: e.latLng.lng(),
     };
 
-    const handleMarkerClick = (place: any) => {
-        setSelectedPlace(place);
-    };
+    modalClick("ModalContent", data, fallback);
+    return;
+    try {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJPdcAX96sDUgROOBQVMLy6_A&key=AIzaSyAqHi-MH3gDZ0uCWYJL9w6Bi0iHtO_Kzx0`
+      );
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+  const mapLocation = {
+    lat: selectedLat,
+    lng: selectedLong,
+  };
 
-    const handleInfoWindowClose = () => {
-        setSelectedPlace(null);
-    };
+  return (
+    isLoaded && (
+      <GoogleMap
+        center={mapLocation}
+        onClick={(e: any) => handleClick(e)}
+        mapContainerClassName="googleMap"
+        onLoad={(map) => {
+          mapRef.current = map;
+          handleZoomChanged();
+          handleMapLoad(map);
+        }}
+        zoom={zoom}
+        onZoomChanged={handleZoomChanged}
+        onUnmount={onUnmount}
+        options={{
+          mapTypeControl: false,
+          zoomControl: window.innerWidth >= 800 ? true : false,
+          fullscreenControl: window.innerWidth >= 800 ? true : false,
+        }}
 
-    const handleZoomChanged = () => {
-        if (mapRef.current) {
-            setZoom(mapRef.current.getZoom());
-        }
-    };
-
-    const markerClick = (e: any) => {
-        // console.log("data of map", e)
-
-    };
-    const handleClick = async (e: any) => {
-        const data = {
-            data_type: "google",
-            place_id: e.placeId,
-            lat: e.latLng.lat(),
-            lng: e.latLng.lng()
-        }
-
-
-        modalClick(
-            "ModalContent",
-            data,
-            fallback
-        )
-        return
-        try {
-            const response = await fetch(
-                `https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJPdcAX96sDUgROOBQVMLy6_A&key=AIzaSyAqHi-MH3gDZ0uCWYJL9w6Bi0iHtO_Kzx0`
-            );
-            const data = await response.json();
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-        } catch (error) {
-            console.log("error", error)
-        }
-    };
-    const mapLocation = {
-        lat: selectedLat,
-        lng: selectedLong,
-    };
-
-    return (
-        isLoaded && (
-            <GoogleMap
-                center={mapLocation}
-                onClick={(e: any) => handleClick(e)}
-                mapContainerClassName="googleMap"
-                onLoad={(map) => {
-                    mapRef.current = map;
-                    handleZoomChanged();
-                    handleMapLoad(map);
-                }}
-                zoom={zoom}
-                onZoomChanged={handleZoomChanged}
-                onUnmount={onUnmount}
-                options={{
-                    mapTypeControl: false,
-                    zoomControl: window.innerWidth >= 800 ? true : false,
-                    fullscreenControl: window.innerWidth >= 800 ? true : false,
-                }}
-
-
-            // onClick={onMapClick}
-            >
-                {userLocation && (
-                    <Marker
-                        position={userLocation}
-                        onClick={() => handleMarkerClick(userLocation)}
-                    />
-                )}
-                <Marker position={mapLocation} onClick={markerClick} />
-                {/* Child components, such as markers, info windows, etc. */}
-                {/* {selectedPlace && ( */}
-                {/* <InfoWindow
+        // onClick={onMapClick}
+      >
+        {userLocation && (
+          <Marker
+            position={userLocation}
+            onClick={() => handleMarkerClick(userLocation)}
+          />
+        )}
+        <Marker position={mapLocation} onClick={markerClick} />
+        {/* Child components, such as markers, info windows, etc. */}
+        {/* {selectedPlace && ( */}
+        {/* <InfoWindow
                     position={center}
                 >
                    
                 </InfoWindow> */}
-                {/* )} */}
-                <></>
-            </GoogleMap>
-        )
-    );
+        {/* )} */}
+        <></>
+      </GoogleMap>
+    )
+  );
 };
 
 export default GoogleMapComp;
