087cefb37cad9d874762b342254b545f7a50f884
naveennkViwell <136687846+naveennkViwell@users.noreply.github.com>
2024-06-17 16:56:37 +0530

    fix(add to list black screen) : updated the logic in instance section

--- src/app/utils/Instance.ts
+++ src/app/utils/Instance.ts
@@ -1,22 +1,19 @@
 import axios from "axios";
 
 const Instance = axios.create({
-  // baseURL: "https://roc-web-app.uc.r.appspot.com",
   baseURL: "https://beta-dot-roc-app-425011.nw.r.appspot.com",
   // baseURL: "http://localhost:8080",
 });
 
 Instance.interceptors.request.use(
   (config) => {
-    const token = window.localStorage.getItem("Token");
     const loginToken = window.localStorage.getItem("loginToken");
-    if (token) {
-      config.headers["x-auth-token"] = token;
-      if (loginToken) {
-        // If loginToken exists, add it to headers
-        config.headers["x-login-token"] = loginToken;
-      }
+
+    if (loginToken) {
+      // If loginToken exists, add it to headers
+      config.headers["x-login-token"] = loginToken;
     }
+
     return config;
   },
   (error) => {
