5428a06cd3df92c18e8118ddbabb406b5ea2eeb4
Your <naveengit2000@gmail.com>
2024-09-05 14:37:31 +0530

    feat: monetag page added

---
+++ src/app/hello-monetag/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const MonetagPage = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        height: "100vh",
+        alignItems: "center",
+        justifyContent: "center",
+      }}>
+      <h1 style={{ color: "white" }}>Hello, Monetag!</h1>
+    </div>
+  );
+};
+
+export default MonetagPage;
