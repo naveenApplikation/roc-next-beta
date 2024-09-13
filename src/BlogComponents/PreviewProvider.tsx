567db3f16c2e4e5555a89610500baf5ee7079097
Your <naveengit2000@gmail.com>
2024-09-13 17:21:26 +0530

    feat: moneytize removed and type added for livequery

--- src/BlogComponents/PreviewProvider.tsx
+++ src/BlogComponents/PreviewProvider.tsx
@@ -1,14 +1,16 @@
-import { getClient } from '@/lib/sanity.client'
-import { LiveQueryProvider } from 'next-sanity/preview'
-import { useMemo } from 'react'
+import { getClient } from "@/lib/sanity.client";
+import { LiveQueryProvider } from "next-sanity/preview";
+import { useMemo } from "react";
 
 export default function PreviewProvider({
   children,
   token,
 }: {
-  children: React.ReactNode
-  token: string
+  children: React.ReactNode;
+  token: string;
 }) {
-  const client = useMemo(() => getClient({ token }), [token])
-  return <LiveQueryProvider client={client}>{children}</LiveQueryProvider>
+  const client = useMemo(() => getClient({ token }), [token]);
+  return (
+    <LiveQueryProvider client={client as any}>{children}</LiveQueryProvider>
+  );
 }
