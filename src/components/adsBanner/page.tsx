2683e760a52b92c4e61fd69761546974427f4bb0
Admin <admin@MacBook-Air.lan>
2024-10-08 11:31:06 +0530

    feat: new ad added

--- src/components/adsBanner/page.tsx
+++ src/components/adsBanner/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Image from "next/image";
-import { BannerDemo, BannerDemo2, RightArow } from "@/app/utils/ImagePath";
+import { BannerDemo, BannerDemo2, BannerDemo3, RightArow } from "@/app/utils/ImagePath";
 import DirectionModalLayout from "@/components/modal/Modal";
 import { useMyContext } from "@/app/Context/MyContext";
 
@@ -52,6 +52,23 @@ const AdBody = styled.div<{
     max-width: ${({ $maxWidth }) => $maxWidth};
   }
 `;
+
+const FullAdBody = styled.div<{
+  $maxWidth: string;
+}>`
+  width: 440px;
+  display: flex;
+ justify-content:center;
+ 
+
+  img {
+    border-radius: 12px 12px 12px 12px;
+  }
+  @media screen and (max-width: 800px) {
+    width: 97%;
+    max-width: ${({ $maxWidth }) => $maxWidth};
+  }
+`;
 const AdContent = styled.div`
   padding: 4px 10px;
   display: flex;
@@ -60,7 +77,7 @@ const AdContent = styled.div`
   width: 100%;
 `;
 const AdText = styled.div`
-  .baaner_heading {
+  .banner_heading {
     font-size: 16px;
     font-weight: 700;
     @media screen and (max-width: 367px) {
@@ -83,55 +100,80 @@ const AdText = styled.div`
   }
 `;
 
+
+
+
+
 interface AdsBannerProps {
   className?: string;
   maxWidth?: string;
 }
+
+// Ad Data
+const adsData = [
+  {
+    type: "existing",
+    image: BannerDemo2,
+    url: "#",
+    heading: "Cafe @ Jersey War Tunnels",
+    text: "Open daily 10am to 5pm",
+  },
+  {
+    type: "new",
+    image: BannerDemo3, // Path to the new ad image
+    url: "https://amzn.to/4dRSj8d", // URL for the new ad
+  },
+];
+
 const AdsBanner: React.FC<AdsBannerProps> = ({
   className = "20px",
   maxWidth = "480px",
 }) => {
-  const [open, setOpen] = useState<boolean>(false);
-  const { showMap, modalClick, modalType, closeModal } = useMyContext();
+  const { modalClick } = useMyContext();
+  const [currentAdIndex, setCurrentAdIndex] = useState(0);
 
-  const handleOpen = () => {
-    setOpen(!open);
-  };
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentAdIndex((prevIndex) => (prevIndex + 1) % adsData.length);
+    }, 5000);
 
-  // console.log("modalTypemodalType" , modalType)
+    return () => clearInterval(interval);
+  }, []);
+
+  const currentAd = adsData[currentAdIndex];
 
   return (
     <>
-      <AdContainer
-        $className={className}
-        $maxWidth={maxWidth}
-        onClick={() => modalClick("adsBanner", "adsBanner")}>
-        <AdBody $maxWidth={maxWidth}>
-          <Image src={BannerDemo2} alt="Advertisement" width={96} height={66} />
-          <AdContent>
-            <AdText>
-              <p className="baaner_heading">Cafe @ Jersey War Tunnels</p>
-              <p className="banner_text">Open daily 10am to 5pm</p>
-            </AdText>
-            <Image src={RightArow} alt="icon" height={20} />
-          </AdContent>
-        </AdBody>
-      </AdContainer>
-      {/* <DirectionModalLayout
-        isOpen={modalType.adsBanner}
-        name="adsBanner"
-        showMap={showMap}
-        onClose={() => closeModal("adsBanner")}
-      >
-        <iframe
-          style={{ border: "none", height: "100%", overflow: 'hidden' }}
-          src="https://hub.roc.je/featured/jersey-war-tunnels-escape-rooms"
-          height="500px"
-          width="100%"
-          title="Jersey War Tunnels Escape Rooms"
-          className="iframe_body"
-        ></iframe>
-      </DirectionModalLayout> */}
+      {currentAd.type === "existing" ? (
+        <AdContainer
+          $className={className}
+          $maxWidth={maxWidth}
+          onClick={() => modalClick("adsBanner", "adsBanner")}
+        >
+          <AdBody $maxWidth={maxWidth}>
+            <Image src={currentAd.image} alt="Advertisement" width={96} height={66} />
+            <AdContent>
+              <AdText>
+                <p className="banner_heading">{currentAd.heading}</p>
+                <p className="banner_text">{currentAd.text}</p>
+              </AdText>
+              <Image src={RightArow} alt="icon" height={20} />
+            </AdContent>
+          </AdBody>
+        </AdContainer>
+      ) : (
+        <AdContainer
+          $className={className} // Set default className
+          $maxWidth={maxWidth} // Set default maxWidth
+      
+          onClick={() => window.open(currentAd.url, "_blank")}
+        >
+            <FullAdBody    $maxWidth={maxWidth}>
+            <Image src={currentAd.image} alt="Full Ad"  height={70}/>           
+          </FullAdBody>
+        
+        </AdContainer>
+      )}
     </>
   );
 };
