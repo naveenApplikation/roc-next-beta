3f81fe9bff732fe298a2095d5e33d345d895dad1
Naveen Kumar <naveengit2000@gmail.com>
2024-11-09 03:27:59 +0530

    fix

--- src/app/xmas/XmasComponents/NewYearEvening.tsx
+++ src/app/xmas/XmasComponents/NewYearEvening.tsx
@@ -5,26 +5,38 @@ import XmasEvent from "./Event";
 import ImageLayout from "./ImageLayout";
 import Places from "./Places";
 
-export default async function NewYearEvening({ads}:{ads:any})
-{
+export default async function NewYearEvening({ ads }: { ads: any }) {
+  const xmasEvents: any[] = (await getXmasEvents(
+    "x-mas-events?limit=true"
+  )) as any;
+  const foodAndDrinkCategory = await getFoodandDrinks("main");
 
-    const xmasEvents:any[]=await getXmasEvents("x-mas-events?limit=true") as any
-    const foodAndDrinkCategory=await getFoodandDrinks('main')
-  
-    return <>
-        <div className="grid grid-cols-1  px-[20px] py-[24px] gap-[16px]">
-                <div className="">
-                    <h1 className="text-[#F40035] text-[32px] font-[900] ">New Year’s Eve</h1>
-                </div>
-                <div>
-                    <p className="text-[16px]  leading-[20px] text-gray-400 font-[500] overflow-hidden  text-ellipsis line-clamp-2">St Helier late night shopping until 9pm on Thursdays through to 23 December.</p>
-                </div>
-                <ImageLayout data={ads}>
-                       {ads.title}
-                </ImageLayout>
-                <XmasEvent nav={"/eventCategory/xmas-events"} data={xmasEvents.slice(0,10)} title="Events"></XmasEvent>
-                <Places nav="screens/Pubs?categoryID=663f68c30c44bd6c026b0fc2"   data={foodAndDrinkCategory[0].category.GoogleFoodAndDrinksList} title="Bars & Nightlife"></Places>
-                 
-         </div>
+  return (
+    <>
+      <div className="grid grid-cols-1  px-[20px] py-[24px] gap-[16px]">
+        <div className="">
+          <h1 className="text-[#F40035] text-[32px] font-[900] ">
+            New Year’s Eve
+          </h1>
+        </div>
+        <div>
+          <p className="text-[16px]  leading-[20px] text-gray-400 font-[500] overflow-hidden  text-ellipsis line-clamp-2">
+            St Helier late night shopping until 9pm on Thursdays through to 23
+            December.
+          </p>
+        </div>
+        <ImageLayout data={ads}>{ads.title}</ImageLayout>
+        <XmasEvent
+          nav={"/eventCategory/xmas-events"}
+          data={xmasEvents.slice(0, 10)}
+          title="Events"
+        ></XmasEvent>
+        <Places
+          nav="screens/Pubs?categoryID=663f68c30c44bd6c026b0fc2"
+          data={foodAndDrinkCategory[0].category.PeopleCommunityList}
+          title="Bars & Nightlife"
+        ></Places>
+      </div>
     </>
+  );
 }
--- src/app/xmas/layout.tsx
+++ src/app/xmas/layout.tsx
@@ -1,43 +1,48 @@
-import './style.css'
-import '@/app/tailwind.css'
-import ScrollLayout from './XmasComponents/ScrollLayout';
-import { LogoNew, MenuIcon } from '../utils/ImagePath';
-import Image from 'next/image';
-import { EventLayout, RocLogoIcon, ShoppingLayout, winterDomes, XmasBgImage } from './utils/XmasImagePath';
-import { url } from 'inspector';
-import Carousel from './XmasComponents/CarouselScreen';
-import { getCarouselData } from './XmasAction';
-import XmasDashboard from './XmasComponents/XmasDashBoard';
-import DashBoardModalScreen from '@/components/dashboard/DashBoardModalScreen';
-export default async function Layout({children}:{children:any}){
-  
+import "./style.css";
+import "@/app/tailwind.css";
+import ScrollLayout from "./XmasComponents/ScrollLayout";
+import { LogoNew, MenuIcon } from "../utils/ImagePath";
+import Image from "next/image";
+import {
+  EventLayout,
+  RocLogoIcon,
+  ShoppingLayout,
+  winterDomes,
+  XmasBgImage,
+} from "./utils/XmasImagePath";
+import { url } from "inspector";
+import Carousel from "./XmasComponents/CarouselScreen";
+import { getCarouselData } from "./XmasAction";
+import XmasDashboard from "./XmasComponents/XmasDashBoard";
+import DashBoardModalScreen from "@/components/dashboard/DashBoardModalScreen";
+export const maxDuration = 300;
 
-       const data:any=await getCarouselData()
-      
-      const slides=[
-            {
-               image:XmasBgImage,
-               
-            }
-            ,
-            {
-                  image:ShoppingLayout
-            },
-            {
-                  image:EventLayout
-            }
-      ]
-      return <>
-      <div className='min-[800px]:hidden fixed z-[1] top-0 h-screen bg-white w-full'></div>
+export default async function Layout({ children }: { children: any }) {
+  const data: any = await getCarouselData();
 
-               <Carousel slides={data}></Carousel>
-            
-                 
-                <ScrollLayout>
-                  <XmasDashboard></XmasDashboard>
-                  </ScrollLayout>
+  const slides = [
+    {
+      image: XmasBgImage,
+    },
+    {
+      image: ShoppingLayout,
+    },
+    {
+      image: EventLayout,
+    },
+  ];
+  return (
+    <>
+      <div className="min-[800px]:hidden fixed z-[1] top-0 h-screen bg-white w-full"></div>
 
-                  {children}    
-                  <DashBoardModalScreen></DashBoardModalScreen>    
-      </>
+      <Carousel slides={data}></Carousel>
+
+      <ScrollLayout>
+        <XmasDashboard></XmasDashboard>
+      </ScrollLayout>
+
+      {children}
+      <DashBoardModalScreen></DashBoardModalScreen>
+    </>
+  );
 }
