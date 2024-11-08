e9b7edcdde41de1fffe2700ea4f8812a930f1c64
Naveen Kumar <naveengit2000@gmail.com>
2024-11-09 03:32:22 +0530

    fix

--- src/app/xmas/iframe/[app]/[id]/page.tsx
+++ src/app/xmas/iframe/[app]/[id]/page.tsx
@@ -3,38 +3,29 @@ import { topSideMenu } from "../../../../utils/data";
 import DashBoardModalScreen from "@/components/dashboard/DashBoardModalScreen";
 import XmasIframeModel from "@/app/xmas/XmasComponents/XmasIframeModel";
 import { getAds, getCarouselData, getHighlights } from "@/app/xmas/XmasAction";
- 
- 
-  
-export default async function Page({params}:{params:{app:string,id:string}}){
- 
- 
-  console.log("title",params)
-  
-   try
-   {
-    
-  const result = await fetch(`${process.env.NEXT_API_URL}/${params.app}`, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-    cache: "force-cache",
-  });
-    const response = await result.json();
-    const filter=response.filter((item)=>{
-      return item._id==params.id
-})
+export const maxDuration = 300;
 
-  console.log(filter,"filter")
-    return <>
-         
- 
-         <XmasIframeModel
-        name="infoApp"
-        title={""}
-      >
-       
-          <div style={{ height: "84vh", width: "100%", overflow: 'hidden' }}>
+export default async function Page({
+  params,
+}: {
+  params: { app: string; id: string };
+}) {
+  try {
+    const result = await fetch(`${process.env.NEXT_API_URL}/${params.app}`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      cache: "force-cache",
+    });
+    const response = await result.json();
+    const filter = response.filter((item) => {
+      return item._id == params.id;
+    });
+
+    return (
+      <>
+        <XmasIframeModel name="infoApp" title={""}>
+          <div style={{ height: "84vh", width: "100%", overflow: "hidden" }}>
             <iframe
               style={{ border: "none", height: "100%", overflow: "hidden" }}
               src={filter[0].iframe_url}
@@ -45,44 +36,34 @@ export default async function Page({params}:{params:{app:string,id:string}}){
               loading="lazy"
             ></iframe>
           </div>
-     
-      </XmasIframeModel>
-      <DashBoardModalScreen></DashBoardModalScreen>
-
-     
-    </>
-}
-catch(error)
-{ 
-    return <>something went wrong!</>
-}
-  
+        </XmasIframeModel>
+        <DashBoardModalScreen></DashBoardModalScreen>
+      </>
+    );
+  } catch (error) {
+    return <>something went wrong!</>;
+  }
 }
 
-
-export async function generateStaticParams()
-{
-
-    const add=(element:any,type:string)=>{
-      if(element.link_type=='iframe')
-        {
-             staticGeneration.push({app:type,id:element._id})
-        }
+export async function generateStaticParams() {
+  const add = (element: any, type: string) => {
+    if (element.link_type == "iframe") {
+      staticGeneration.push({ app: type, id: element._id });
     }
-    const staticGeneration:any=[]
-    const carousel=await getCarouselData() as any[]
-    carousel.forEach(element => {
-          add(element,'carousel')
-    });
-    const ads=await getAds() as any[]
-    ads.forEach(element => {
-      add(element,'advertisement')
-});
-    const highlight=await getHighlights() as any[]
-    highlight.forEach(element => {
-      add(element,'highlight')
-});
-     console.log(staticGeneration)
-    return staticGeneration
-   
+  };
+  const staticGeneration: any = [];
+  const carousel = (await getCarouselData()) as any[];
+  carousel.forEach((element) => {
+    add(element, "carousel");
+  });
+  const ads = (await getAds()) as any[];
+  ads.forEach((element) => {
+    add(element, "advertisement");
+  });
+  const highlight = (await getHighlights()) as any[];
+  highlight.forEach((element) => {
+    add(element, "highlight");
+  });
+  console.log(staticGeneration);
+  return staticGeneration;
 }
