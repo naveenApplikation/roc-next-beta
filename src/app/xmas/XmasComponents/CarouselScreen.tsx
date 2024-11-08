01f0dcdbed5237c99bcd32ae99a9303f5a60075c
Naveen Kumar <naveengit2000@gmail.com>
2024-11-08 23:26:11 +0530

    fix: max correction

--- src/app/xmas/XmasComponents/CarouselScreen.tsx
+++ src/app/xmas/XmasComponents/CarouselScreen.tsx
@@ -1,10 +1,10 @@
-'use client'
- import Image from "next/image";
+"use client";
+import Image from "next/image";
 import { Fragment, useEffect, useState } from "react";
 import { RocLogoIcon } from "../utils/XmasImagePath";
 import { MenuIcon } from "@/app/utils/ImagePath";
 
-import '../style.css'
+import "../style.css";
 import { transform } from "lodash";
 import { useMyContext } from "@/app/Context/MyContext";
 import { useRouter } from "next-nprogress-bar";
@@ -21,64 +21,57 @@ const filterUrls = (ImageUrlData: any) => {
         if (url && (url.endsWith(".jpg") || url.endsWith(".png"))) {
           imageUrls.push(convertGCSUrl(url));
         } else {
-          imageUrls.push(
-            fallback.src
-          ); // Push default image URL if URL is not valid
+          imageUrls.push(fallback.src); // Push default image URL if URL is not valid
         }
       } catch (error) {
         console.error("Error parsing JSON:", error);
-        imageUrls.push(
-          fallback.src          ); // Push default image URL if JSON parsing fails
+        imageUrls.push(fallback.src); // Push default image URL if JSON parsing fails
       }
     } else {
-      imageUrls.push(
-        fallback.src        ); // Push default image URL if item is undefined
+      imageUrls.push(fallback.src); // Push default image URL if item is undefined
     }
   });
   return imageUrls;
 };
 
 const Carousel = ({ slides }) => {
- const router=useRouter()
+  const router = useRouter();
 
- const {modalClick}=useMyContext()
+  const { modalClick } = useMyContext();
 
-  const navigate=(type:string,item:any)=>{
-    console.log(item)
-    switch(type)
-    {
-       case 'event':
-        
-           const filteredUrls = filterUrls([item.event_id.acf.header_image_data]);
-           console.log(filteredUrls)
-           modalClick(
-               "eventListing",
-               {
-                   acf:item.event_id.acf
-               },
-               filteredUrls[0] ? filteredUrls[0] : fallback
-             )
-           break;
-       case 'iframe':
-            router.push(`/xmas/iframe/carousel/${item?._id}`)
-            break;
-       case 'eventCategory':
-            router.push(`/eventCategory/${item?.category_id}`)
-         
-            break;
-       case 'place':
-              modalClick(
-                "eventListing",
-                item,
-                item?.data_type === "google" ? item?.photoUrl : fallback
-              )
-            break;
-       default:
-          router.push('/xmas')     
+  const navigate = (type: string, item: any) => {
+    console.log(item);
+    switch (type) {
+      case "event":
+        const filteredUrls = filterUrls([item.event_id.acf.header_image_data]);
+        console.log(filteredUrls);
+        modalClick(
+          "eventListing",
+          {
+            acf: item.event_id.acf,
+          },
+          filteredUrls[0] ? filteredUrls[0] : fallback
+        );
+        break;
+      case "iframe":
+        router.push(`/xmas/iframe/carousel/${item?._id}`);
+        break;
+      case "eventCategory":
+        router.push(`/eventCategory/${item?.category_id}`);
+
+        break;
+      case "place":
+        modalClick(
+          "eventListing",
+          item,
+          item?.data_type === "google" ? item?.photoUrl : fallback
+        );
+        break;
+      default:
+        router.push("/xmas");
     }
-  }
-  
-   
+  };
+
   const [currentIndex, setCurrentIndex] = useState(0);
   const slideInterval = 5000; // 4 seconds
 
@@ -95,7 +88,9 @@ const Carousel = ({ slides }) => {
   }, [currentIndex, slides.length]);
 
   const goToPrevious = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 0 ? slides.length - 1 : prevIndex - 1));
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? slides.length - 1 : prevIndex - 1
+    );
   };
 
   const goToNext = () => {
@@ -103,7 +98,7 @@ const Carousel = ({ slides }) => {
   };
 
   const handleTouchStart = (e) => {
-    console.log(e)
+    console.log(e);
     touchStartX = e.changedTouches[0].clientX;
   };
 
@@ -114,123 +109,130 @@ const Carousel = ({ slides }) => {
 
   const handleSwipe = () => {
     if (touchStartX - touchEndX > 50) {
-      
       goToNext();
     }
     if (touchStartX - touchEndX < -50) {
-     
       goToPrevious();
     }
   };
 
-  const RenderImage=()=>{
+  const RenderImage = () => {
+    return (
+      <>
+        <div
+          className="carousel-container absolute w-full h-full top-0 max-w-[100%] mx-auto overflow-hidden"
+          onTouchStart={handleTouchStart}
+          onTouchEnd={handleTouchEnd}
+          onClick={() =>
+            navigate(slides[currentIndex].link_type, slides[currentIndex])
+          }
+        >
+          <div
+            className="carousel-inner flex transition-transform duration-1000 ease-in-out"
+            style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+          >
+            {slides.map((slide, index) => (
+              <div key={index} className="flex-shrink-0 max-[800px]:w-[100%]">
+                <Image
+                  height={510}
+                  width={1000}
+                  src={slide.img_url}
+                  alt=""
+                  className="w-full h-[510px] object-cover"
+                  priority
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </>
+    );
+  };
 
-   
-     return <>
-      <div 
-  className="carousel-container absolute w-full h-full top-0 max-w-[100%] mx-auto overflow-hidden"
-  onTouchStart={handleTouchStart}
-  onTouchEnd={handleTouchEnd}
-  onClick={() => navigate(slides[currentIndex].link_type, slides[currentIndex])}
->
-  <div className="carousel-inner flex transition-transform duration-1000 ease-in-out" 
-    style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
-    {slides.map((slide, index) => (
-      <div 
-        key={index}
-        className="flex-shrink-0 max-[800px]:w-[100%]"
-      >
-        <Image
-          height={510}
-          width={1000}
-          src={slide.img_url}
-          alt=""
-          className="w-full h-[510px] object-cover"
-          priority
-        />
-      </div>
-    ))}
-  </div>
-</div>
-
-     </>
-  }
-
-  return <>
-     <div className='min-[800px]:hidden fixed  top-0 h-[550px] w-full z-[1]'>
-             <RenderImage></RenderImage>
-             {/* <button onClick={goToPrevious} className="absolute  left-0 top-[45%] transform -translate-y-1/2 bg-black/30 text-white p-2">
+  return (
+    <>
+      <div className="min-[800px]:hidden fixed  top-0 h-[550px] w-full z-[1]">
+        <RenderImage></RenderImage>
+        {/* <button onClick={goToPrevious} className="absolute  left-0 top-[45%] transform -translate-y-1/2 bg-black/30 text-white p-2">
         ‹
       </button>
       <button onClick={goToNext} className="absolute right-0   top-[45%] transform -translate-y-1/2 bg-black/30 text-white p-2">
         ›
       </button> */}
-               <div className='w-full fixed top-0 flex justify-between items-center  px-[24px] pt-[30px]'>
-                  
-                  <Image
-                   src={RocLogoIcon}
-                   alt=''
-                   height={500}
-                   width={500}
-                   className='w-[117px] h-[48px]'
-                  >
-         
-                  </Image>
-                  <div className='flex bg-[#F40035] rounded-[10px]  justify-between gap-[8px] items-center px-[10px] py-[9px]'>
-                        <p className='text-[15px] text-white font-[700]'>Xmas Guide</p>
-                        <Image
-                         onClick={() => modalClick("createAccountModal")}
-                          src={MenuIcon}
-                          alt=''
-                          height={500}
-                          width={500}
-                          className='relative h-[16px] w-[20px]'
-                        ></Image>
-                  </div>
-        
-               </div>
-               
-               <div 
-                          style={{ color:`'#fff` }}   onTouchStart={handleTouchStart}
-                          onTouchEnd={handleTouchEnd}
-                          onClick={()=>navigate(slides[currentIndex].link_type,slides[currentIndex])}
-                        className='backdropEffect'>
-                              <p className={`font-[400] ${currentIndex==1?'text-black':'text-white'} text-[18px]`} >{slides[currentIndex].date}</p>
-                              <p className={`font-[550] ${currentIndex==1?'text-black':'text-white'} text-[22px] leading-[25px]`}>{slides[currentIndex].title.split(' ').map((item,index)=>{
-                                   
-                                    if(index==2 && currentIndex!=2)
-                                    {
-                                       return <span key={index}>{item+" "}<br/> </span>
-                                    }
-                                    else
-                                    {
-                                       return <Fragment key={index}>{item+" "}</Fragment>
-                                    }
-                              })}</p>
-                              <div>
-                                    <p className={`text-[16px] ${currentIndex==1?'text-black':'text-white'} font-[500]`}>View Details</p>
-                              </div>
-                              <div className='flex flex-row gap-[5px] p-[4px] w-[100px]'>
-                                {slides.map((_,index)=>{
-                                    return  <div key={index} className={`${index === currentIndex ? 'bg-white':'bg-gray-500 '} w-full h-[3px] rounded-[100px]`}></div>
-                                    
-                                })}
-                                    {/* <div className='bg-white w-full h-[3px] rounded-[100px]'></div>
+        <div className="w-full fixed top-0 flex justify-between items-center  px-[24px] pt-[30px]">
+          <Image
+            src={RocLogoIcon}
+            alt=""
+            height={500}
+            width={500}
+            className="w-[117px] h-[48px]"
+            onClick={() => router.push("/")}
+          ></Image>
+          <div
+            onClick={() => modalClick("createAccountModal")}
+            className="flex bg-[#F40035] rounded-[10px]  justify-between gap-[8px] items-center px-[10px] py-[9px]"
+          >
+            <p className="text-[15px] text-white font-[700]">Xmas Guide</p>
+            <Image
+              src={MenuIcon}
+              alt=""
+              height={500}
+              width={500}
+              className="relative h-[16px] w-[20px]"
+            ></Image>
+          </div>
+        </div>
+
+        <div
+          style={{ color: `'#fff` }}
+          onTouchStart={handleTouchStart}
+          onTouchEnd={handleTouchEnd}
+          onClick={() =>
+            navigate(slides[currentIndex].link_type, slides[currentIndex])
+          }
+          className="backdropEffect"
+        >
+          <p className={`font-[400] ${"text-white"} text-[18px]`}>
+            {slides[currentIndex].date}
+          </p>
+          <p
+            className={`font-[550] ${"text-white"} text-[22px] leading-[25px]`}
+          >
+            {slides[currentIndex].title.split(" ").map((item, index) => {
+              if (index == 2 && currentIndex != 2) {
+                return (
+                  <span key={index}>
+                    {item + " "}
+                    <br />{" "}
+                  </span>
+                );
+              } else {
+                return <Fragment key={index}>{item + " "}</Fragment>;
+              }
+            })}
+          </p>
+          <div>
+            <p className={`text-[16px] ${"text-white"} font-[500]`}>
+              View Details
+            </p>
+          </div>
+          <div className="flex flex-row gap-[5px] p-[4px] w-[100px]">
+            {slides.map((_, index) => {
+              return (
+                <div
+                  key={index}
+                  className={`${index === currentIndex ? "bg-white" : "bg-gray-500 "} w-full h-[3px] rounded-[100px]`}
+                ></div>
+              );
+            })}
+            {/* <div className='bg-white w-full h-[3px] rounded-[100px]'></div>
                                     <div className='bg-gray-500 w-full h-[3px] rounded-[100px]'></div>
                                     <div className='bg-gray-500 w-full h-[3px] rounded-[100px]'></div> */}
-                              </div>
-                      
-                </div>
-              
-                 
-                  
-                </div>
-  
-   
-    
-  </>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
-
-
-export default Carousel
+export default Carousel;
--- src/app/xmas/XmasComponents/EventsAndActivities.tsx
+++ src/app/xmas/XmasComponents/EventsAndActivities.tsx
@@ -1,97 +1,107 @@
+import Image from "next/image";
+import {
+  dining,
+  drinks,
+  FrontArrow,
+  hearts,
+  RedArrow,
+  EventLayout,
+  smily,
+  winterDomes,
+} from "../utils/XmasImagePath";
+import { backArrow } from "@/app/utils/ImagePath";
+import XmasMenu from "./XmasMenu";
+import XmasEvent from "./Event";
+import CommonButton from "./CommonButton";
+import ListingItems from "./ListingItems";
+import ImageLayout from "./ImageLayout";
+import { getCategory } from "../XmasAction";
+import { Fragment } from "react";
+import { handleEventEncoding } from "@/app/utils/commanFun";
 
-import Image from "next/image"
-import { dining, drinks, FrontArrow, hearts, RedArrow, EventLayout, smily, winterDomes } from "../utils/XmasImagePath"
-import { backArrow } from "@/app/utils/ImagePath"
-import XmasMenu from "./XmasMenu"
-import XmasEvent from "./Event"
-import CommonButton from "./CommonButton"
-import ListingItems from "./ListingItems"
-import ImageLayout from "./ImageLayout"
-import { getCategory } from "../XmasAction"
-import { Fragment } from "react"
-import { handleEventEncoding } from "@/app/utils/commanFun"
 export default async function EventAndActivities({ ads }: { ads: any }) {
+  const events: any = await getCategory("main");
+  const categoryList: any = await getCategory("secondary");
 
-    const events: any = await getCategory('main')
-    const categoryList: any = await getCategory('secondary')
-    
-    const EventItems = () => {
-        return events.map((data, index) => {
-            return <Fragment key={index}>
-                <XmasEvent nav={'/eventCategory/'+handleEventEncoding("encode", data?.listName)} data={data.EventList} title={data?.listName}></XmasEvent>
-            </Fragment>
-        })
-    }
-
-    return <>
-
-
-        <div id="events" className="grid grid-cols-1  px-[20px] py-[24px] gap-[16px]">
-            <div className="">
-                <h1 className="text-[#F40035] text-[32px] font-[900] ">Events & Activities</h1>
-            </div>
-            <ImageLayout data={ads}>
-                {ads?.title.split(':')[0]}  <br /> {ads?.title.split(':')[1]}
-
-            </ImageLayout>
-
-
-
-            <EventItems></EventItems>
-            <ListingItems title="event" data={categoryList}></ListingItems>
-            <CommonButton>All Christmas Events</CommonButton>
-        </div>
-
-    </>
-}
-
-
-
-const data = [{
-    content: "Dining & Tasting",
-    image: dining
-},
-{
-    content: "Family-Friendly",
-    image: smily
-},
-{
-    content: "Community & Charity",
-    image: hearts
-},
-{
-    content: "New Years Eve",
-    image: drinks
-}
-
-]
-const EventListing = () => {
-    return <>
-        <div className="py-[16px] px-[24px] bg-[#F5F5F5] rounded-[16px]">
-            {data.map((item: any) => {
-                return <div key={item} className="flex justify-between items-center  border border-x-0 border-t-0 border-b-gray-300 " >
-                    <div className="relative border-none flex items-center grow gap-[16px] py-[12px]">
-                        <Image
-                            alt=""
-                            src={item.image}
-                        ></Image>
-
-                        <p className="absolute text-[18px] left-[50px] font-[400]">{item.content}</p>
-
-                    </div>
-                    <Image
-                        height={10}
-                        width={8}
-                        src={RedArrow}
-                        alt=""
-
-                    >
-                    </Image>
-                </div>
-
-            })
+  const EventItems = () => {
+    return events.map((data, index) => {
+      return (
+        <Fragment key={index}>
+          <XmasEvent
+            nav={
+              "/eventCategory/" + handleEventEncoding("encode", data?.listName)
             }
-        </div>
-    </>
+            data={data.EventList}
+            title={data?.listName}
+          ></XmasEvent>
+        </Fragment>
+      );
+    });
+  };
 
+  return (
+    <>
+      <div
+        id="events"
+        className="grid grid-cols-1  px-[20px] py-[24px] gap-[16px]"
+      >
+        <div className="">
+          <h1 className="text-[#F40035] text-[32px] font-[900] ">
+            Events & Activities
+          </h1>
+        </div>
+        <ImageLayout data={ads}>
+          {ads?.title.split(":")[0]} <br /> {ads?.title.split(":")[1]}
+        </ImageLayout>
+
+        <EventItems></EventItems>
+        <ListingItems title="event" data={categoryList}></ListingItems>
+        <CommonButton>All Christmas Events</CommonButton>
+      </div>
+    </>
+  );
 }
+
+const data = [
+  {
+    content: "Dining & Tasting",
+    image: dining,
+  },
+  {
+    content: "Family-Friendly",
+    image: smily,
+  },
+  {
+    content: "Community & Charity",
+    image: hearts,
+  },
+  {
+    content: "New Years Eve",
+    image: drinks,
+  },
+];
+const EventListing = () => {
+  return (
+    <>
+      <div className="py-[16px] px-[24px] bg-[#F5F5F5] rounded-[16px]">
+        {data.map((item: any) => {
+          return (
+            <div
+              key={item}
+              className="flex justify-between items-center  border border-x-0 border-t-0 border-b-gray-300 "
+            >
+              <div className="relative border-none flex items-center grow gap-[16px] py-[12px]">
+                <Image alt="" src={item.image}></Image>
+
+                <p className="absolute text-[18px] left-[50px] font-[400]">
+                  {item.content}
+                </p>
+              </div>
+              <Image height={10} width={8} src={RedArrow} alt=""></Image>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
