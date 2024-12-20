4bf68d4d44371372f78f8bdd49065186f09d2f52
Naveen Kumar <naveengit2000@gmail.com>
2024-12-21 00:08:04 +0530

    bug fix

--- next.config.js
+++ next.config.js
@@ -44,7 +44,7 @@ const nextConfig = {
     ],
   },
   reactStrictMode: false,
-  staticPageGenerationTimeout: 2000,
+  staticPageGenerationTimeout: 5000,
   redirects,
 };
 
--- src/app/eventCategory/[event]/page.tsx
+++ src/app/eventCategory/[event]/page.tsx
@@ -11,22 +11,18 @@ import React from "react";
 export const maxDuration = 300;
 // Generate static paths for dynamic routes
 export async function generateStaticParams() {
-  try {
-    const category = await getCategory("event-list");
+  const category = await getCategory("event-list");
+  const staticGeneration = [];
+  // const staticGeneration = category.map((item: any) => {
+  //   return {
+  //     event: handleEventEncoding("encode", item.listName),
+  //   };
+  // });
 
-    const staticGeneration = category.map((item: any) => {
-      return {
-        event: handleEventEncoding("encode", item.listName),
-      };
-    });
+  // Add "upcoming" as a static path
+  staticGeneration.push({ event: "upcoming" });
 
-    // Add "upcoming" as a static path
-    staticGeneration.push({ event: "upcoming" });
-
-    return staticGeneration;
-  } catch (error) {
-    console.log("hey boy");
-  }
+  return staticGeneration;
 }
 
 export default async function Page({ params }: { params: { event: string } }) {
