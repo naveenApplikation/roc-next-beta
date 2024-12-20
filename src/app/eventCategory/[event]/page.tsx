17b492ca843c410debcb413764cb5f4fbc86fcf9
Naveen Kumar <naveengit2000@gmail.com>
2024-12-21 00:53:42 +0530

    event bug fix

--- src/app/eventCategory/[event]/page.tsx
+++ src/app/eventCategory/[event]/page.tsx
@@ -64,7 +64,7 @@ export default async function Page({ params }: { params: { event: string } }) {
     <>
       <HeaderScreen title={"Events"} />
       <EventPage
-        urlData={data}
+        urlData={data.slice(0, 10)}
         urlTitle={title}
         type="eventCategory"
         slug={params.event}
